df8d4d5d8731bcb590dc5f1e1772b58cb566da9c
vandana vemula <vandana.vemula@i95dev.com>
2022-09-02 12:56:34 +0530

    Create Opportunity

---
+++ src/main/java/io/elastic/i95dev/marketo/actions/CreateOpportunity.java
@@ -0,0 +1,62 @@
+package io.elastic.i95dev.marketo.actions;
+
+import java.io.StringReader;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonString;
+
+import org.apache.http.client.methods.HttpPost;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.elastic.i95dev.marketo.utils.HttpClientUtils;
+import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Function;
+import io.elastic.api.Message;
+
+public class CreateOpportunity implements Function {
+	private static final Logger logger = LoggerFactory.getLogger(CreateOpportunity.class);
+
+	@Override
+	public void execute(ExecutionParameters parameters) {
+		final JsonObject configuration = parameters.getConfiguration();
+		final JsonObject body = parameters.getMessage().getBody();
+		JsonString externalOpportunityId = body.getJsonString("externalOpportunityId"); // 19UYA31581L000000
+		JsonString name = body.getJsonString("name"); // Chairs
+		JsonString description = body.getJsonString("description"); // Chairs
+		JsonString amount = body.getJsonString("amount"); // 1604.47
+		JsonString source = body.getJsonString("source");
+		JsonString action = body.getJsonString("action"); // 1604.47
+		JsonString dedupeBy = body.getJsonString("dedupeBy");
+
+		JsonObject dateobject = Json.createObjectBuilder().add("externalOpportunityId",externalOpportunityId ).add("name", name).add("description", description)
+				.add("amount", amount).add("source", source).build();
+		JsonArray input = Json.createArrayBuilder().add(dateobject).build();
+
+		JsonObject requestObj = Json.createObjectBuilder().add("dedupeBy", dedupeBy).add("action", action)
+				.add("input", Json.createArrayBuilder().add(dateobject)).build();
+
+		String endpoint = "/rest/v1/opportunities.json";
+		HttpPost req = HttpClientUtils.createPostObjectRequest(configuration, endpoint, requestObj);
+		try {
+			String res = HttpClientUtils.sendRequest(req);
+			JsonReader jsonReader = Json.createReader(new StringReader(res));
+			JsonObject object = jsonReader.readObject();
+			jsonReader.close();
+
+			final Message response = new Message.Builder().body(object).build();
+
+			parameters.getEventEmitter().emitData(response);
+
+			logger.info("Finished execution");
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
